68c46418cd358ab19cca11d5549ec52cab5353fb
Adam Feher <fhradm@gmail.com>
2018-09-10 15:06:03 +0200

    Implemented posting new Score and getting all scores

--- src/controllers/score.controller.ts
+++ src/controllers/score.controller.ts
@@ -1,5 +1,8 @@
 import {Request, Response } from 'express';
-import {controller, httpGet, httpPost, interfaces} from "inversify-express-utils";
+import {controller, httpGet, httpPost, interfaces, requestBody} from "inversify-express-utils";
+import {ScoreRequest} from "../models/scoreRequest";
+import {ScoreResponse} from "../models/scoreResponse";
+import {Score, ScoreModel} from "../models/score";
 
 @controller('/score')
 export class ScoreController implements interfaces.Controller {
@@ -7,12 +10,14 @@ export class ScoreController implements interfaces.Controller {
     constructor() { }
 
     @httpGet('/')
-    private get(req: Request, res: Response) {
-        
+    private async get(req: Request, res: Response) {
+        return await Score.find();//.sort((obj1: any, obj2: any) => obj1.steps-obj2.steps);
     }
 
     @httpPost('/')
-    private post(req: Request, res: Response) {
-
+    private post(@requestBody() req: ScoreRequest, res: ScoreResponse) {
+        const score = new Score ({steps: req.steps, seconds: req.seconds, name: req.name});
+        score.save();
+        return {position: 4}
     }
 }
