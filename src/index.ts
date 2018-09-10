404af2e40172a179ec6a2434fa8e07f421653b89
Adam Feher <fhradm@gmail.com>
2018-09-10 15:42:53 +0200

    Added Score and Uuid service into IoC

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import "./controllers/game.controller";
 import "./controllers/score.controller";
 import {GameService} from "./services/game.service";
 import {connect} from "mongoose";
+import {ScoreService} from "./services/score.service";
+import {UuidGenerator} from "./utilities/uuidGenerator";
 
 
 const port = process.env.PORT || 4000;
@@ -13,6 +15,8 @@ connect(process.env.MONGODB_URL!, {useNewUrlParser: true});
 
 const container = new Container(); // IoC container
 container.bind<GameService>('GameService').to(GameService).inSingletonScope();
+container.bind<ScoreService>('ScoreService').to(ScoreService).inSingletonScope();
+container.bind<UuidGenerator>('UuidGenerator').to(UuidGenerator).inSingletonScope();
 
 const server = new InversifyExpressServer(container);
 
