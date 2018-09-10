adb96524655e9789361dc898848f24199fdb9771
Adam Feher <fhradm@gmail.com>
2018-09-10 14:18:49 +0200

    Binded GameService into inversify IoC

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import {Container} from "inversify";
 import {InversifyExpressServer} from "inversify-express-utils";
 import "./controllers/game.controller";
 import "./controllers/score.controller";
+import {GameService} from "./services/game.service";
 
 
 const port = process.env.PORT || 4000;
 
 const container = new Container(); // IoC container
-
+container.bind<GameService>('GameService').to(GameService).inSingletonScope();
 
 const server = new InversifyExpressServer(container);
 
