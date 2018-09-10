2274cd3a1ceda5b3fa49f0a598876fed6c798ccb
Adam Feher <fhradm@gmail.com>
2018-09-10 14:48:15 +0200

    Implemented schema for score

--- src/models/score.ts
+++ src/models/score.ts
@@ -1,5 +1,24 @@
-export interface Score {
+import {Document, model, Schema} from 'mongoose';
+
+export interface ScoreModel extends Document {
     steps: number;
     seconds: number;
     name: string;
 }
+
+const ScoreSchema = new Schema({
+    steps: {
+        type: Number,
+        requred: true,
+    },
+    seconds: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+export const Score = model<ScoreModel>('Score', ScoreSchema);
