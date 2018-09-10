ae50e8a1bbfe2d48143a2496d07bb9acd4ec69a2
Adam Feher <fhradm@gmail.com>
2018-09-10 15:48:00 +0200

    Added difficulty validation into new game request

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,6 +1,7 @@
-import {controller, httpGet, interfaces, requestParam} from "inversify-express-utils";
+import {controller, httpGet, interfaces, requestParam, response} from "inversify-express-utils";
 import {inject} from "inversify";
 import {GameService} from "../services/game.service";
+import {Response} from "express";
 
 @controller('/game')
 export class GameController implements interfaces.Controller {
@@ -8,7 +9,10 @@ export class GameController implements interfaces.Controller {
     constructor(@inject('GameService') private gameService: GameService) { }
 
     @httpGet('/:size')
-    private newGame(@requestParam('size') size: number) {
-        return this.gameService.newGame(size)
+    private newGame(@requestParam('size') size: number, @response() resp: Response) {
+        if (size < 2 || size > 10) {
+            return resp.status(400).send(Error("Invalid difficulty. Valid difficulties are between 2-10").message);
+        }
+        return this.gameService.newGame(size);
     }
 }
